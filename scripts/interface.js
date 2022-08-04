610b830c0482af66883a9490ba1be177e22ea127
gitlhermesantiago <98118165+gitlhermesantiago@users.noreply.github.com>
2022-08-04 17:16:05 -0300

    Add files via upload

---
+++ scripts/interface.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+    })
+})
+
+
+function handleClick(event){
+   
+    let square= event.target;
+    let position=square.id;
+
+   if( handleMove(position)){
+    setTimeout(()=>{alert("O jogo Acabou - O Vencedor foi o jogador "+playerTime)},10);
+    
+   }
+    updateSquares(position);
+
+}
+
+
+
+function updateSquares(position){
+    let square=document.getElementById(position.toString())
+    let symbol=board[position] 
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
